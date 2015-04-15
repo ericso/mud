644ff5a0d8f95e14445f82a0a55e2490a9e62a69
Eric So <me@eric.so>
2015-04-15 12:22:03 -0700

    Fix up page formatting, moved setup code setup.js

--- ui/static/js/game.js
+++ ui/static/js/game.js
@@ -1,27 +1,12 @@
 $(function() {
-
-  // Set up a global CSRF token for all AJAX calls
-  // var CSRF_TOKEN = document.getElementById('csrf_token').value;
-  $.ajaxSetup({
-    headers: { 'X-CSRFToken': CSRF_TOKEN }
-  });
-
-  // Set up the 'enter' keypress event
-  $('input').keyup(function(e) {
-    if (e.keyCode == 13) {
-      $(this).trigger('enter');
-    }
-  });
-
-  $(document).ready(function(e) {
-    $("body").css("paddingBottom", $("#id-console-in").get("offsetHeight"));
-  });
+  'use strict';
 
   // Add Backbone views
   app.consoleInput = new App.Views.ConsoleInput();
   app.consoleOutput = new App.Views.ConsoleOutput();
 
+  // Testing code
   // app.testModels();
-  app.testCollections();
+  // app.testCollections();
 
 });
--- ui/static/js/views.js
+++ ui/static/js/views.js
@@ -1,28 +1,6 @@
 $(function() {
   'use strict';
 
-  var COMMANDS = [
-    {
-      name: "do_stuff",
-      handler: doStuff
-    }, {
-      name: "greet",
-      handler: function(args) {
-        outputToConsole("Hello " + args[0] + ", welcome to Console.");
-      }
-    }
-  ];
-
-  function doStuff(args) {
-    outputToConsole("I'll just echo the args: " + args);
-  }
-
-  function outputToConsole(text) {
-    var p = $("<p>" + text + "</p>");
-    $("#id-console-out").append(p);
-    p.scrollIntoView();
-  }
-
   // Backbone View for console input
   App.Views.ConsoleInput = Backbone.View.extend({
     el: '#id-console-in',
@@ -42,13 +20,13 @@ $(function() {
       var command = parts[0];
       var args = parts.length > 1 ? parts.slice(1, parts.length) : [];
       inField.val("");
-      for (var i = 0; i < COMMANDS.length; i++) {
-        if (command === COMMANDS[i].name) {
-          COMMANDS[i].handler(args);
+      for (var i = 0; i < app.commands.length; i++) {
+        if (command === app.commands[i].name) {
+          app.commands[i].handler(args);
           return;
         }
       }
-      outputToConsole("Unsupported Command: " + command);
+      app.outputToConsole("Unsupported Command: " + command);
     }
 
   });
