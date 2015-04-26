fb20a170bd43409ecbd0070d9fe3a427cd15edfa
Eric So <me@eric.so>
2015-04-25 23:49:25 -0700

    Remove old migrations

--- game/migrations/0001_initial.py
+++ game/migrations/0001_initial.py
@@ -11,12 +11,26 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Dungeon',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.TextField(default=b'Dungeon')),
+                ('description', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='WorldNode',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('x_pos', models.IntegerField()),
-                ('y_pos', models.IntegerField()),
-                ('text', models.TextField()),
+                ('x', models.IntegerField()),
+                ('y', models.IntegerField()),
+                ('unvisited', models.TextField()),
+                ('visited', models.TextField()),
+                ('look', models.TextField()),
+                ('dungeon', models.ForeignKey(to='game.Dungeon')),
             ],
             options={
             },
